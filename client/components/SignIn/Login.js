7f6aaee0af25d42311387d47ed142ea53b9a8a80
Vincent <vkl2109@columbia.edu>
2023-02-18 07:53:59 -0500

    added scrollview and safeareaview

--- client/components/SignIn/Login.js
+++ client/components/SignIn/Login.js
@@ -1,19 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import * as React from 'react';
+
+const screenWidth = Dimensions.get('window').width; 
 
 export default function Login () {
     return (
-        <View style={styles.container}>
-            <Text>Login</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollview} >
+                <Text>Login</Text>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    scrollview: {
+        flexGrow: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        width: screenWidth
+    }
 });
--- client/components/SignIn/Signup.js
+++ client/components/SignIn/Signup.js
@@ -1,19 +1,30 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ScrollView, Dimensions } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 import * as React from 'react';
+
+const screenWidth = Dimensions.get('window').width; 
 
 export default function Signup () {
     return (
-        <View style={styles.container}>
-            <Text>Signup</Text>
-        </View>
+        <SafeAreaView style={styles.container}>
+            <ScrollView contentContainerStyle={styles.scrollview} >
+                <Text>Signup</Text>
+            </ScrollView>
+        </SafeAreaView>
     )
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    scrollview: {
+        flexGrow: 1, 
+        justifyContent: 'center', 
+        alignItems: 'center', 
+        width: screenWidth
+    }
 });
